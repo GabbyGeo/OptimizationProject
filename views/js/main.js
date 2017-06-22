57f6622a458eb213f966c7537f4cbaacbb16bdba
GabbyGeo <gabriellejf@gmail.com>
2017-06-22 10:57:30 -0600

    Add files via upload

--- views/js/main.js
+++ views/js/main.js
@@ -423,32 +423,7 @@ var resizePizzas = function(size) {
   changeSliderLabel(size);
 
    // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  /*function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
-        case "1":
-          return 0.25;
-        case "2":
-          return 0.3333;
-        case "3":
-          return 0.5;
-        default:
-          console.log("bug in sizeSwitcher");
-      }
-    }
-
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
-  }*/
-
-  // Iterates through pizza elements on the page and changes their widths
+   //optimized
   function changePizzaSizes(size) {
       switch(size) {
         case "1":
@@ -516,7 +491,7 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 
 // Moves the sliding background pizzas based on scroll position
 
-var items = document.querySelectorAll('.mover');
+var items = document.getElementsByClassName('.mover');
 
 function updatePositions() {
   frame++;
